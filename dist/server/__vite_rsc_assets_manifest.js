bd13ae4194b889bb4ba74487af03bd7fddb4fab2
likecimu98-sys <likecimu98@gmail.com>
2026-07-28 14:33:57 -0700

    Add files via upload

---
+++ dist/server/__vite_rsc_assets_manifest.js
@@ -0,0 +1,47 @@
+export default {
+  "bootstrapScriptContent": "import(\"/assets/index-B19C8_tM.js\")",
+  "clientReferenceDeps": {
+    "6efdf509a785": {
+      "js": [
+        "/assets/page-BzmDjYCY.js",
+        "/assets/rolldown-runtime-BYbx6iT9.js",
+        "/assets/framework-DkySH6nJ.js",
+        "/assets/index-B19C8_tM.js"
+      ],
+      "css": []
+    },
+    "8f7ba5393f33": {
+      "js": [
+        "/assets/index-B19C8_tM.js",
+        "/assets/rolldown-runtime-BYbx6iT9.js",
+        "/assets/framework-DkySH6nJ.js"
+      ],
+      "css": []
+    },
+    "ee6cb3d92c61": {
+      "js": [
+        "/assets/layout-segment-context-ZD2Vzadz.js",
+        "/assets/rolldown-runtime-BYbx6iT9.js",
+        "/assets/index-B19C8_tM.js",
+        "/assets/framework-DkySH6nJ.js"
+      ],
+      "css": []
+    },
+    "1e6222746775": {
+      "js": [
+        "/assets/index-B19C8_tM.js",
+        "/assets/rolldown-runtime-BYbx6iT9.js",
+        "/assets/framework-DkySH6nJ.js"
+      ],
+      "css": []
+    }
+  },
+  "serverResources": {
+    "app/layout.tsx": {
+      "js": [],
+      "css": [
+        "/assets/index-DwOkMW0V.css"
+      ]
+    }
+  }
+}
